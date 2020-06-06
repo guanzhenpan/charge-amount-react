d36b7028ceed63b898cfc58326d2de328e04a520
panguanzhen <panguanzhen@qq.com>
2020-06-06 23:00:38 +0800

    deploy

---
+++ precache-manifest.6fe8441d48c9395d8f1a5d0767ead5ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7228facb0af42b4a6af57a1f3dbaaef9",
+    "url": "/charge-amount-react/index.html"
+  },
+  {
+    "revision": "6856ec084fcf5e71fd11",
+    "url": "/charge-amount-react/static/css/main.1e0d8429.chunk.css"
+  },
+  {
+    "revision": "951ec870e90a967b1b72",
+    "url": "/charge-amount-react/static/js/2.daac4cb7.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/charge-amount-react/static/js/2.daac4cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6856ec084fcf5e71fd11",
+    "url": "/charge-amount-react/static/js/main.cbf2e617.chunk.js"
+  },
+  {
+    "revision": "16ad8c01b4f97fd1b05a",
+    "url": "/charge-amount-react/static/js/runtime-main.9cbf28b1.js"
+  }
+]);
